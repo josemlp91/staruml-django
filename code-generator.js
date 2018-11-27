9fda845444d3c9b37ec8b33076ebd67a7a5888df
abhishekrai-kiwi <abhishek.rai@kiwitech.com>
2018-11-27 17:13:24 +0530

    Fixed typo
    Foreign key was misspelled.

--- code-generator.js
+++ code-generator.js
@@ -331,7 +331,7 @@ class DjangoCodeGenerator {
         if (['0..*', '1..*', '*'].includes(asso.end1.multiplicity.trim()) && asso.end2.multiplicity == "1"){
           var refObjName = asso.end2.reference.name;
           var var_name = asso.name;
-          codeWriter.writeLine(var_name + " = models.ForeingKey('" + asso.end2.reference.name + "'" + tags_str +", on_delete=models.PROTECT)");
+          codeWriter.writeLine(var_name + " = models.ForeignKey('" + asso.end2.reference.name + "'" + tags_str +", on_delete=models.PROTECT)");
         }
 
         if (['0..*', '1..*', '*'].includes(asso.end1.multiplicity.trim()) && ['0..*', '1..*', '*'].includes(asso.end2.multiplicity.trim())){
