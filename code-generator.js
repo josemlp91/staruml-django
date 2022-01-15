5fabca64b2e992a0b43d8383a655461337219c9e
Charles Hill <quantumcharleshill@gmail.com>
2022-01-15 13:36:10 +1100

    Small grammar changes

--- code-generator.js
+++ code-generator.js
@@ -543,14 +543,14 @@ function generate (baseModel, basePath, options) {
   fullPath = basePath + '/' + baseModel.name.toLowerCase();
   if (!fs.existsSync(fullPath)){
     fs.mkdirSync(fullPath);
-  }else{
-    var buttonId = app.dialogs.showConfirmDialog("Exist a folder with the same name, overwrite?");
+  } else {
+    var buttonId = app.dialogs.showConfirmDialog("A folder exists with the same name, overwrite?");
     if (buttonId === 'ok') {
       deleteFolderRecursive(fullPath);
       fs.mkdirSync(fullPath);
-      app.dialogs.showInfoDialog("New folder creaded.");
+      app.dialogs.showInfoDialog("New folder created.");
     } else {
-      app.dialogs.showErrorDialog("Canceled operation by user.");
+      app.dialogs.showErrorDialog("Operation canceled by user.");
     }
   }
   baseModel.ownedElements.forEach(child => {
